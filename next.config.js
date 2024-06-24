b9a6a990f2089c9c5ee0ae0f2e09dac3304f0b93
masaoskari <masaoskari.linna@gmail.com>
2024-06-24 22:09:48 +0300

    Add basepath back

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   output: "export",
+  basePath: "/algoritmi-akatemia",
 };
 
 module.exports = withMDX(nextConfig);
