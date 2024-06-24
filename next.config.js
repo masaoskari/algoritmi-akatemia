7f63d151e3d3a23a9c259230b328a8860d79a2fd
masaoskari <masaoskari.linna@gmail.com>
2024-06-24 22:13:51 +0300

    try to get styles working

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   output: "export",
-  basePath: "/algoritmi-akatemia",
 };
 
 module.exports = withMDX(nextConfig);
