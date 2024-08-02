04103f2e11c488acdf083dd2178150144efbf0a4
masaoskari <masaoskari.linna@gmail.com>
2024-08-02 20:01:48 +0300

    Add use media query hook

---
+++ src/hooks/useMediaQuery.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+// Code originates from: https://samuelkraft.com/blog/responsive-animation-framer-motion
+export function useMediaQuery(query: string): boolean {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+
+    const listener = () => {
+      setMatches(media.matches);
+    };
+
+    if (typeof media.addEventListener === "function") {
+      media.addEventListener("change", listener);
+    } else {
+      media.addListener(listener);
+    }
+
+    return () => {
+      if (typeof media.removeEventListener === "function") {
+        media.removeEventListener("change", listener);
+      } else {
+        media.removeListener(listener);
+      }
+    };
+  }, [matches, query]);
+
+  return matches;
+}
+
+export const useIsLargeScreen = () => useMediaQuery("(min-width: 768px)");
