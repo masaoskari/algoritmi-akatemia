2d0b6563f6da8f146d36a144ea7f24f9bead6e51
masaoskari <masaoskari.linna@gmail.com>
2024-08-04 12:55:43 +0300

    Add code editor and exercise components

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,14 +1,73 @@
-export const CodeEditor = () => {
+"use client";
+import usePyodide from "@/hooks/usePyodide";
+import Editor, { OnMount } from "@monaco-editor/react";
+import { useRef, useState } from "react";
+import { editor } from "monaco-editor";
+
+type CodeEditorProps = {
+  onCodeExecution?: (output: string) => void;
+  answer?: string | undefined;
+  height?: number; // Height of the editor in pixels
+};
+
+export const CodeEditor = ({
+  onCodeExecution,
+  height = 300,
+  answer = undefined,
+}: CodeEditorProps) => {
+  const { runPythonCode } = usePyodide();
+
+  const [output, setOutput] = useState<string>("");
+  const editorRef = useRef<editor.IStandaloneCodeEditor | null>(null);
+
+  const handleEditorDidMount: OnMount = (editor) => {
+    editorRef.current = editor;
+  };
+
+  const runCode = async () => {
+    if (editorRef.current) {
+      const code = editorRef.current.getValue();
+      const output = await runPythonCode(code);
+      setOutput(output);
+      if (onCodeExecution) onCodeExecution(output);
+    }
+  };
+
   return (
-    <div>
-      <h2 className="mt-4">Harjoitus</h2>
-      <div className="rounded shadow-lg">
-        <div className="rounded-tl-md rounded-tr-md bg-blue-600 flex justify-between items-center p-4">
-          <p className="text-white">Nimi</p>
-          <p className="text-white">Pisteet: 10</p>
+    <div className="rounded shadow-lg flex flex-col">
+      <div className="p-4 pr-8" style={{ height: `${height}px` }}>
+        <Editor
+          onMount={handleEditorDidMount}
+          defaultLanguage="python"
+          options={{
+            minimap: { enabled: false },
+            scrollBeyondLastLine: false,
+            overviewRulerLanes: 0,
+            hideCursorInOverviewRuler: true,
+          }}
+        />
+      </div>
+      <button
+        className="w-24 bg-blue-600 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded ml-4"
+        onClick={runCode}
+      >
+        Suorita
+      </button>
+      <div
+        style={{ whiteSpace: "pre-wrap" }}
+        className="m-4 min-h-[200px] shadow-lg"
+      >
+        <div className="flex justify-between">
+          <p className="font-semibold px-4">Konsoli</p>
+          {answer && output ? (
+            <p className="font-semibold px-4">
+              {output && answer && output.trim() === answer.trim()
+                ? "👍 Oikein"
+                : "❌ Väärin"}
+            </p>
+          ) : null}
         </div>
-        <div className="pl-4">Tähän tulee harjoituksen kuvaus</div>
-        <div className="bg-black h-48 w-96"></div>
+        <div className="px-4 pb-4">{output}</div>
       </div>
     </div>
   );
--- src/components/Excercise.tsx
+++ src/components/Excercise.tsx
@@ -1,79 +1,48 @@
 "use client";
-import usePyodide from "@/hooks/usePyodide";
-import Editor, { OnMount } from "@monaco-editor/react";
-import { useRef } from "react";
-import * as monaco from "monaco-editor";
+import React, { useState } from "react";
+import { CodeEditor } from "./CodeEditor";
 
 type ExcerciseProps = {
+  editorHeight?: number;
   name: string;
-  children: string;
+  children: React.ReactNode;
   points: number;
   answer: string;
 };
 
 export const Excercise = ({
+  editorHeight = 200,
   name,
   children,
   points,
   answer,
 }: ExcerciseProps) => {
-  const { runPythonCode, output } = usePyodide();
-  const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
+  const [isCorrect, setIsCorrect] = useState<boolean>(false);
 
-  const handleEditorDidMount: OnMount = (editor) => {
-    editorRef.current = editor;
+  const handleCodeExecutionOutput = (output: string) => {
+    setIsCorrect(output.trim() === answer.trim());
   };
 
-  const runCode = async () => {
-    if (editorRef.current) {
-      const code = editorRef.current.getValue();
-      console.log("Running code:", code);
-      await runPythonCode(code);
-      console.log("Output:", output);
-    }
-  };
   return (
     <>
       <h2 className="mt-4">Harjoitus</h2>
       <div className="rounded shadow-lg">
         <div className="rounded-tl-md rounded-tr-md bg-blue-600 flex justify-between items-center p-4">
-          <p className="text-white">Nimi</p>
-          <p className="text-white">Pisteet: 10</p>
+          <p className="text-white font-bold">{name}</p>
+          {/* TODO: Points needs to be store and get from server side. */}
+          <p className="text-white font-bold">{`Pisteet: ${
+            isCorrect ? points : "0"
+          } / ${points}`}</p>
         </div>
-        <div className="pl-4">{children}</div>
-
-        <Editor
-          className="bg-black"
-          width={"500px"}
-          options={{
-            minimap: { enabled: false },
-            glyphMargin: false,
-            // Disable unnecessary editor features
-            scrollBeyondLastLine: false,
-            overviewRulerBorder: false, // Hide overview ruler border
-            overviewRulerLanes: 0, // Hide overview ruler lanes
-            hideCursorInOverviewRuler: true, // Hide cursor in overview ruler
-          }}
-          height={"200px"}
-          defaultLanguage="python"
-          onMount={handleEditorDidMount}
+        <div className="p-4">{children}</div>
+        <p className="p-4">
+          Kirjoita ohjelmasi alle ja tarkista se "Suorita" painikkeella:
+        </p>
+        <CodeEditor
+          height={editorHeight}
+          answer={answer}
+          onCodeExecution={handleCodeExecutionOutput}
         />
-
-        <button
-          className="bg-blue-600 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded ml-4"
-          onClick={runCode}
-        >
-          Suorita
-        </button>
-
-        <div
-          style={{ whiteSpace: "pre-wrap" }}
-          className="m-4 min-h-[200px] shadow-lg"
-        >
-          <p className="font-semibold pl-4">Konsoli</p>
-          <div className="px-4 pb-4">{output}</div>
-        </div>
-        <br />
       </div>
     </>
   );
