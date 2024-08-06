f5e2e769507ca85fd14e7c96b2b3a0e38d9ccce1
masaoskari <masaoskari.linna@gmail.com>
2024-08-06 17:39:33 +0300

    Simplify sidepanel

--- src/app/materiaali/[...slug]/layout.tsx
+++ src/app/materiaali/[...slug]/layout.tsx
@@ -1,14 +1,16 @@
-import SidePanel from "@/components/SidePanel";
 import "@/app/globals.css";
+import { getAllContentFrontMatters } from "@/lib/mdxUtils";
+import SidePanel from "@/components/SidePanel";
 
 export default function ContentPageLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const frontMatters = getAllContentFrontMatters();
   return (
     <div className="flex md:flex-row flex-col h-screen overflow-hidden">
-      <SidePanel />
+      <SidePanel frontMatters={frontMatters} />
       <div className="w-full flex flex-col p-4 md:p-8 md:pt-4 overflow-y-auto">
         {children}
       </div>
