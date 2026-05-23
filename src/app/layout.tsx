5b5f883139674068991a68b01d4e20813e5830ae
Matti <masaoskari.linna@gmail.com>
2026-05-23 11:27:56 +0300

    Add correct favicon.ico

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Algoritmi akatemia",
   description: "Algoritmi akatemia nettisivut koodauksen opiskeluun.",
+  icons: {
+    icon: "/a_a_logo.svg",
+  },
 };
 
 export default function RootLayout({
